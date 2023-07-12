cdbb87849aebfe70069e1e58c087590ec72e96c1
Rerurate514 <rerurateyuto@gmail.com>
2023-07-12 23:43:54 +0900

    ゲーム進行クラスの作成

---
+++ src/logic/GameCoodinator.ts
@@ -0,0 +1,36 @@
+class GameCoodinator{
+    /**
+     * gameStart
+     */
+    public gameStart() {
+        
+    }
+
+    /**
+     * gameFinish
+     */
+    public gameFinish() {
+        
+    }
+
+    /**
+     * gamePause
+     */
+    public gamePause() {
+        
+    }
+
+    /**
+     * gameReset
+     */
+    public gameReset() {
+        
+    }
+
+    /**
+     * gameEmergencyStop
+     */
+    public gameEmergencyStop() {
+        
+    }
+}
