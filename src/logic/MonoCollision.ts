2c714a7c5a5d46e0900631ee53023d2b7759a33c
Rerurate514 <rerurateyuto@gmail.com>
2023-07-20 00:24:05 +0900

    createMonoLowerCollision関数の完成

--- src/logic/MonoCollision.ts
+++ src/logic/MonoCollision.ts
@@ -1,25 +1,60 @@
 type Field = number[][];
+type CollisionRef = {
+    coodinate:{
+        monoBasis: {
+            x : number,
+            y : number
+        },
+        monoLimit: {
+            x : number,
+            y : number
+        }
+    },
+    monoLowerCollision : number[]
+};
 export class MonoCollision{
+    
     /**
      * ## この関数はmonoの衝突判定を出す関数です。
-     * @date 2023/7/18 - 10:33:27
+     * @date 2023/7/18 - 15:37:31
      *
-     * @private
+     * @public
+     * @param {Field} _movingMonoField
+     * @param {Field} _placedMonoField
+     * @param {CollisionRef} _collisionRefY
      * @returns {boolean}
      */
-    private isBottomCollision() : boolean{
+    public isBottomCollision(
+        _placedMonoField : Field,
+        _collisionRef: CollisionRef
+        ) : boolean{
+
+        let basis = {
+            x : _collisionRef.coodinate.monoBasis.x,
+            y : _collisionRef.coodinate.monoBasis.y,
+        };
+
+        let limit = {
+            x : _collisionRef.coodinate.monoLimit.x,
+            y : _collisionRef.coodinate.monoLimit.y,
+        };
+            
         
 
-        return true;
+        return false;
     }
 
-    private calculateMonoLowerLimit(_mono: Field): number{
-        let lowerLimit = 0;
-        for(let v = 0; v < _mono.length; v++){
-            for(let h = 0; h < _mono[v].length; h++){
-                if(_mono[v][h] !== 0) { lowerLimit = v; }
+    public createMonoLowerCollision(_mono : Field): number[]{
+        let monoLowerCollision = new Array(4).fill(-1);
+    
+        for (let v = 1; v < _mono.length + 1; v++) {
+            for (let h = 0; h < _mono.length; h++) {
+                if(_mono[v - 1][h] !== 0){
+                    monoLowerCollision[h] = v;
+                }
             }
-        }
-        return lowerLimit;
+        }  
+    
+        return monoLowerCollision;
     }
 }
